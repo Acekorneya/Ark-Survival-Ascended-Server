464093129372e676f04aea98301a9109e26d0edd
ace <ace.korneya@gmail.com>
2024-03-08 22:09:58 -0800

    fix

--- POK-manager.sh
+++ POK-manager.sh
@@ -1483,7 +1483,7 @@ update_manager_and_instances() {
     echo "---- ARK server files not found. Installing server files using SteamCMD -----"
     ensure_steamcmd_executable # Make sure SteamCMD is executable
     if sudo "${BASE_DIR%/}/config/POK-manager/steamcmd/steamcmd.sh" +login anonymous +force_install_dir "${BASE_DIR%/}/ServerFiles/arkserver" +app_update 2430930 validate +quit; then
-      echo "----- ARK server files installed successfully -----"
+      echo "----- ARK server files installed successfully build: ${latest_build_id} -----"
       # Move the appmanifest_2430930.acf file to the correct location
       if [ -f "${BASE_DIR%/}/ServerFiles/arkserver/steamapps/appmanifest_2430930.acf" ]; then
         cp "${BASE_DIR%/}/ServerFiles/arkserver/steamapps/appmanifest_2430930.acf" "${BASE_DIR%/}/ServerFiles/arkserver/"
